b6ed91307a603704a8416536e1a23e40709ebfec
Artyom Mazurenko <artemmazurenko71@gmail.com>
2025-04-09 17:05:47 +0300

    lab6 2-4: functors, agents

---
+++ AgentProcessor/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+
+type TaskMessage =
+    | SayHello of string
+    | Multiply of int * int
+    | Terminate
+
+let workerAgent = 
+    MailboxProcessor.Start(fun inbox ->
+        let rec workerLoop () = 
+            async {
+                let! msg = inbox.Receive()
+                
+                match msg with
+                | SayHello name -> 
+                    Console.WriteLine("Привет, {0}!", name)
+                    return! workerLoop()
+                
+                | Multiply (x, y) ->
+                    Console.WriteLine("Произведение {0} и {1}: {2}", x, y, (x * y))
+                    return! workerLoop()
+                
+                | Terminate ->
+                    Console.WriteLine("Агент завершает свою работу.")
+                    return ()
+            }
+        workerLoop ()
+    )
+
+workerAgent.Post(SayHello "Артём")
+workerAgent.Post(Multiply(3, 9))
+workerAgent.Post(Terminate)
+
+Threading.Thread.Sleep(1000)
